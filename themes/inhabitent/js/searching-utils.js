46734d68e0c2025b43651f0021666bec62bea20a
Agata <agata@swistowski.org>
2018-11-21 00:08:56 +0000

    fix search toggle functionality

--- themes/inhabitent/js/searching-utils.js
+++ themes/inhabitent/js/searching-utils.js
@@ -1,10 +1,15 @@
 function showSearch() {
-    let search_field = document.getElementById('search-box');
-
-    if ( search_field.style.display == 'none' ) {
+    const search_field = document.getElementById('search-box');
+    console.log(search_field.style.display);
+    if ( !search_field.style.display || search_field.style.display == 'none' ) {
         search_field.style.display = 'inline';
+        search_field.focus();
     }
     else {
+        search_field.onblur = hideSearch();
+    }
+    
+    function hideSearch() {
         search_field.style.display = 'none';
     }
 }
--- themes/inhabitent/single.php
+++ themes/inhabitent/single.php
@@ -13,6 +13,12 @@ get_header(); ?>
             <?php while ( have_posts() ) : the_post(); ?>
                 <?php get_template_part( 'template-parts/content', 'single' ); ?>
 
+                <div class="buttons">
+                    <button type="button" class="black-btn"><i class="fa fa-facebook"></i>Like</button>
+                    <button type="button" class="black-btn"><i class="fa fa-twitter"></i>Tweet</button>
+                    <button type="button" class="black-btn"><i class="fa fa-pinterest"></i>Pin</button>
+                </div>
+
                 <?php
                     // If comments are open or we have at least one comment, load up the comment template.
                     if ( comments_open() || get_comments_number() ) :
